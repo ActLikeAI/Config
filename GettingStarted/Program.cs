33c86ae1ece7ff0922a66e36478a1029618a470a
Atila Čeki <7001148+ActLikeAI@users.noreply.github.com>
2020-02-12 22:03:36 +0100

    Sample console application added.

---
+++ GettingStarted/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ActLikeAI.Config;
+
+namespace GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config.Load("Sample.cfg", @"ActLikeAI/ConfigTest");
+
+            Console.WriteLine($"Size.Width = {Config.Get("Size.Width")}");
+            Console.WriteLine($"Size.Height = {Config.Get("Size.Height")}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Colors.Foreground:R = {Config.Get("Colors.Foreground:R")}");
+            Console.WriteLine($"Colors.Foreground:G = {Config.Get("Colors.Foreground:G")}");
+            Console.WriteLine($"Colors.Foreground:B = {Config.Get("Colors.Foreground:B")}");
+
+            Config.Save();
+        }
+    }
+}
