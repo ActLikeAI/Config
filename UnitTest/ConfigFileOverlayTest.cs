c64baaab67b6452e48ddf1709975c76f3f7f677e
Atila Čeki <7001148+ActLikeAI@users.noreply.github.com>
2020-02-26 21:42:29 +0100

    Added unit test project to the solution. GettingStarted removed, as tests are better showcase of usage.

---
+++ UnitTest/ConfigFileOverlayTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ActLikeAI.Config.UnitTest
+{
+    [TestClass]
+    public class ConfigFileOverlayTest
+    {
+        private ConfigFile config;
+        private string tempDir;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            XElement root = new XElement("Editor",
+                                new XElement("MainWindow",
+                                    new XElement("Font",
+                                        new XElement("Family", "Consolas"),
+                                        new XElement("Size", "16")),
+                                    new XElement("Colors",
+                                        new XElement("Background",
+                                            new XAttribute("R", "128"),
+                                            new XAttribute("G", "176"),
+                                            new XAttribute("B", "208")))
+                                    ));
+
+            tempDir = Path.GetTempPath();
+            using (var writer = File.CreateText(Path.Combine(tempDir, "Editor.cfg")))
+            {
+                root.Save(writer);
+            }
+            
+
+            config = ConfigFile.Load("Editor.cfg", new XmlConfigProvider())
+                        .AddLocation(tempDir, true);
+        }
+
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            string overlay = Path.Combine(tempDir, "Editor.cfg");
+            if (File.Exists(overlay))
+                File.Delete(overlay);
+        }
+
+
+        [TestMethod]
+        public void GetUnmodified()
+        {
+            Assert.AreEqual("0", config.Get("MainWindow.Colors.Foreground.B"));
+        }
+
+
+        [TestMethod]
+        public void GetModified()
+        {
+            Assert.AreEqual("Consolas", config.Get("MainWindow.Font.Family"));
+            Assert.AreEqual("176", config.Get("MainWindow.Colors.Background.G"));
+        }
+
+
+        [TestMethod]
+        public void Save()
+        {
+            Assert.AreEqual("0", config.Get("MainWindow.Colors.Foreground.B"));
+
+            config.Set("MainWindow.Colors.Foreground.B", "11");
+            config.Save();
+
+            config = ConfigFile.Load("Editor.cfg", new XmlConfigProvider())
+                        .AddLocation(tempDir);
+
+            Assert.AreEqual("11", config.Get("MainWindow.Colors.Foreground.B"));
+        }
+    }
+}
---
+++ UnitTest/ConfigFileTest.cs
@@ -0,0 +1,70 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ActLikeAI.Config.UnitTest
+{
+    [TestClass]
+    public class ConfigFileTest
+    {
+        private ConfigFile config;
+        
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            config = ConfigFile.Load(@"Editor.cfg", new XmlConfigProvider());
+        }
+
+
+        [TestMethod]        
+        public void GetSimpleOption()
+        {
+            Assert.AreEqual(@".\Assets", config.Get("AssetsDir"));
+        }
+
+
+        [TestMethod]
+        public void GetNestedOption()
+        {
+            Assert.AreEqual("640", config.Get("MainWindow.Size.Width"));
+        }
+
+
+        [TestMethod]
+        public void GetAttribute()
+        {
+            Assert.AreEqual("215", config.Get("MainWindow.Colors.Foreground.G"));            
+        }
+
+
+        [TestMethod]
+        public void GetAsInt()
+        {
+            Assert.AreEqual(255, config.Get<int>("MainWindow.Colors.Foreground.R"));
+        }
+
+
+        [TestMethod]
+        public void GetAsDouble()
+        {
+            Assert.AreEqual(18.5, config.Get<double>("MainWindow.Font.Size"), 1E-6);
+        }
+
+
+        [TestMethod]
+        public void Set()
+        {
+            Assert.AreEqual("640", config.Get("MainWindow.Size.Width"));
+            config.Set("MainWindow.Size.Width", "768");
+            Assert.AreEqual("768", config.Get("MainWindow.Size.Width"));
+        }
+
+
+        [TestMethod]
+        public void SetAsInt()
+        {
+            Assert.AreEqual("480", config.Get("MainWindow.Size.Height"));
+            config.Set("MainWindow.Size.Height", "768");
+            Assert.AreEqual("768", config.Get("MainWindow.Size.Height"));
+        }
+    }
+}
