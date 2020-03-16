fc5a42af1df47c1607c66b60620e9bc44693f907
Atila Čeki <7001148+ActLikeAI@users.noreply.github.com>
2020-03-16 22:33:42 +0100

    Added new project for IniConfigProvider. Implemented Load method for it. Test for XmlConfigProvider renamed for clarity.

---
+++ Config.Ini/IniConfigProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using IniParser;
+
+namespace ActLikeAI.Config.Ini
+{
+    /// <summary>
+    /// Represents an INI configuration file format.
+    /// </summary>
+    public class IniConfigProvider : IConfigProvider
+    {
+        /// <summary>
+        /// Loads an INI configuration file.
+        /// </summary>
+        /// <param name="file">File to load.</param>
+        /// <returns>Root node of the loaded configuration tree.</returns>
+        public ConfigNode Load(string file)
+        {
+            var parser = new FileIniDataParser();
+            var data = parser.ReadFile(file);
+           
+            var root = new ConfigNode(null, Path.GetFileNameWithoutExtension(file));
+            foreach (var global in data.Global)
+                root.Children.Add(new ConfigNode(root, global.KeyName, global.Value));
+
+            foreach (var section in data.Sections)
+            {
+                var child = new ConfigNode(root, section.SectionName);
+                foreach (var key in section.Keys)
+                    child.Children.Add(new ConfigNode(child, key.KeyName, key.Value));
+
+                root.Children.Add(child);
+            }
+
+            return root;
+        }
+
+
+        /// <summary>
+        /// Saves the configuration file.
+        /// </summary>
+        /// <param name="root">Root node of the configuration tree to save.</param>
+        /// <param name="saveLocation">Full path to the location where to save the file.</param>
+        public void Save(ConfigNode root, string saveLocation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTest/ConfigFileIniTest.cs
@@ -0,0 +1,70 @@
+﻿using ActLikeAI.Config.Ini;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ActLikeAI.Config.UnitTest
+{
+    [TestClass]
+    public class ConfigFileIniTest
+    {
+        private ConfigFile config;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            config = new ConfigFile(@"Editor.ini", new IniConfigProvider());
+        }
+
+
+        [TestMethod]
+        public void GetSimpleOption()
+        {
+            Assert.AreEqual(@".\Assets", config.Get("AssetsDir"));
+        }
+
+
+        [TestMethod]
+        public void GetNestedOption()
+        {
+            Assert.AreEqual("640", config.Get("WindowSize.Width"));
+            Assert.AreEqual("480", config.Get("WindowSize.Height"));
+        }
+
+
+        [TestMethod]
+        public void GetAsInt()
+        {
+            Assert.AreEqual(12, config.Get<int>("AlternateFont.Size"));
+        }
+
+
+        [TestMethod]
+        public void GetAsDouble()
+        {
+            Assert.AreEqual(14.5, config.Get<double>("Font.Size"), 1E-6);
+        }
+
+
+        [TestMethod]
+        public void Set()
+        {
+            Assert.AreEqual("Consolas", config.Get("AlternateFont.Family"));
+            config.Set("AlternateFont.Family", "Comic Sans");
+            Assert.AreEqual("Comic Sans", config.Get("AlternateFont.Family"));
+        }
+
+
+        [TestMethod]
+        public void SetAsInt()
+        {
+            Assert.AreEqual("640", config.Get("WindowSize.Width"));
+            config.Set("WindowSize.Width", "320");
+            Assert.AreEqual("320", config.Get("WindowSize.Width"));
+        }
+
+
+    }
+}
